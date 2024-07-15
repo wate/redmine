753cf2396227b82bdfe0db48e4feefe7d72b6836
wate <pc.poisoning@gmail.com>
2024-07-16 04:40:37 +0900

    update

--- .devcontainer/post_start.sh
+++ .devcontainer/post_start.sh
@@ -3,10 +3,6 @@
 # pipx upgrade-all
 # bundle update
 
-if [ -e "${PWD}/.envrc" ]; then
-  direnv allow
-fi
-
 if [ -e "${PWD}/.devcontainer/post_start.yml" ]; then
   cd "${PWD}/.devcontainer" || exit 1
   ansible-playbook post_start.yml -i localhost, -c local
